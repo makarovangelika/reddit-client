5d2999f7d77e77c6ed08a49ff5fbd9f644011c85
Angelika Makarova <esipovaangelika@mail.ru>
2022-05-13 16:19:38 +0300

    Style subreddit menu for mobile

--- src/components/SubredditMenu/SubredditMenu.js
+++ src/components/SubredditMenu/SubredditMenu.js
@@ -5,20 +5,34 @@ import './SubredditMenu.css';
 
 export function SubredditMenu() {
     let [subreddits, setSubreddits] = useState([]);
+    let [openedMenu, setOpenedMenu] = useState(false);
+    const toggleMenu = () => {
+        setOpenedMenu(!openedMenu);
+    }
     useEffect(() => {
         getSubreddits()
             .then(response => setSubreddits(response))
     });
     return (
-        <nav className='subreddit-menu'>
-            {subreddits.map(subreddit => {
-                return <NavLink to={`/${subreddit.display_name}`} key={subreddit.id}>
-                    {subreddit.icon_img ?
-                        <img className='subreddit-icon' src={subreddit.icon_img} alt='icon of the subreddit' /> :
-                        <div className='subreddit-icon'></div>}
-                    <span>{subreddit.display_name}</span>
-                </NavLink>
-            })}
-        </nav>
+        <div className={`subreddit-menu ${openedMenu ? 'opened' : ''}`}>
+            <div onClick={toggleMenu} className='toggler'>
+                <div className='open'>
+                    <img src='/burger-menu.svg' alt='hamburger menu icon' />
+                </div>
+                <div className='close'>
+                    <img src='/close-icon.svg' alt='icon to close menu' />
+                </div>
+            </div>
+            <nav className='subreddit-links'>
+                {subreddits.map(subreddit => {
+                    return <NavLink onClick={toggleMenu} to={`/${subreddit.display_name}`} key={subreddit.id}>
+                        {subreddit.icon_img ?
+                            <img className='subreddit-icon' src={subreddit.icon_img} alt='icon of the subreddit' /> :
+                            <div className='subreddit-icon'></div>}
+                        <span>{subreddit.display_name}</span>
+                    </NavLink>
+                })}
+            </nav>
+        </div>
     )
 }
